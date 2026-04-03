e44a573dd878e71867c238c77b87307d45fd62e5
agamemnon140 <agamemnon140@proton.me>
2026-04-03 17:10:03 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'bbb531-v2';
+const URLS = ['./', './index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(URLS);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      if (cached) return cached;
+      return fetch(e.request).then(function(response) {
+        var clone = response.clone();
+        caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, clone);
+        });
+        return response;
+      });
+    }).catch(function() {
+      return caches.match('./');
+    })
+  );
+});
